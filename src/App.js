8db7f43d4ac0dec7a6e1a841b124f9835d3cdcc8
gkseogus <fbzbffldj@gmail.com>
2022-09-05 15:49:11 +0900

    220905_apply to Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./AllRountes";
 import MainNavBar from "./components/MainNavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MainNavBar />
+        <Footer />
         {/* <AllRoutes /> */}
       </BrowserRouter>
     </div>
