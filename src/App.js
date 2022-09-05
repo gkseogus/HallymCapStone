7b4afe55f27cb5f354a85d4af3fa9dc1a035b010
DogGang <rlaehgusqp@naver.com>
2022-09-05 19:41:45 +0900

    220905_fix to footer position

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./AllRountes";
 import MainNavBar from "./components/MainNavBar";
 import Footer from "./components/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MainNavBar />
+        <Home />
         <Footer />
         {/* <AllRoutes /> */}
       </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
