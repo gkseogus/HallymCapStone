b8a4b2b59136302eeecfc28a778b39f6763410bc
gkseogus <fbzbffldj@gmail.com>
2022-09-05 13:05:32 +0900

    220905_add to mainNavbar

---
+++ src/components/MainNavBar/index.js
@@ -0,0 +1,119 @@
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { AiOutlineGlobal } from "react-icons/ai";
+import styled from "styled-components";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+// import i18next from "../MainNavBar/I18n/index";
+
+const Contain = styled.nav`
+  width: 100vw;
+  height: 80px;
+  top: 0;
+  background-color: #d9d9d9;
+  display: flex;
+`;
+
+const LogoContain = styled.div`
+  width: 50px;
+  height: 50px;
+  margin: auto;
+  margin-left: 20%;
+  background-color: #999999;
+  border-radius: 10px;
+`;
+
+const LogoText = styled.h2`
+  color: #ffffff;
+  font-size: 16px;
+`;
+
+const NavLinkContain = styled(Link)`
+  text-decoration: none;
+`;
+
+const NavLinkText = styled.h2`
+  width: 160px;
+  height: 48px;
+  margin: 13px;
+  justify-content: center;
+  align-items: center;
+  background-color: #999999;
+  display: grid;
+  color: #ffffff;
+  font-size: 16px;
+  border-radius: 30px;
+`;
+
+const IconContain = styled.div`
+  width: 25px;
+  height: 25px;
+  margin-left: 3%;
+  margin-top: 30px;
+`;
+
+const DropdownContain = styled.div`
+  width: 60px;
+  height: 25px;
+  margin-top: 13px;
+  margin-right: 4%;
+`;
+
+const MainNavBar = () => {
+  const [dropCount, setDropCount] = useState("");
+
+  const handleChange = (event) => {
+    setDropCount(event.target.value);
+  };
+
+  // 다국어 처리 기능 함수
+  //   const { t } = useTranslation();
+
+  //   const clickHandler = (lang) => {
+  //     i18next.changeLanguage(lang);
+  //   };
+
+  return (
+    <Contain>
+      <LogoContain>
+        {/* Logo는 예시 -> 나중에 이미지로 교체 */}
+        <LogoText>Logo</LogoText>
+      </LogoContain>
+      <NavLinkContain to="/home">
+        <NavLinkText>Get Started</NavLinkText>
+      </NavLinkContain>
+      <NavLinkContain to="/contact">
+        <NavLinkText>Contact Us</NavLinkText>
+      </NavLinkContain>
+      <IconContain>
+        <AiOutlineGlobal />
+      </IconContain>
+      <DropdownContain>
+        <Select
+          value={dropCount}
+          onChange={handleChange}
+          displayEmpty
+          style={{
+            width: "60.8px",
+            height: "25px",
+            fontSize: "5px",
+            margin: "13px",
+            background: "#none",
+          }}
+        >
+          <MenuItem value="">
+            <em>KO</em>
+            {/* <em onClick={() => clickHandler("ko")}>KO</em> */}
+          </MenuItem>
+          {/* <MenuItem value={10} onClick={() => clickHandler("en")}>
+            EN
+          </MenuItem> */}
+          <MenuItem value={10}>EN</MenuItem>
+        </Select>
+      </DropdownContain>
+    </Contain>
+  );
+};
+
+export default MainNavBar;
