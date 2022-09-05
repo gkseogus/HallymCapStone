41d3651ae1111f922c3ca6b7a14dc94c56dcabd1
gkseogus <fbzbffldj@gmail.com>
2022-09-05 13:05:49 +0900

    220905_add to i18n function

---
+++ src/components/MainNavBar/I18n/index.js
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import TranslationEn from "./translatonEn.json";
+import TranslationKo from "./translationKo.json";
+
+const resource = {
+  en: {
+    translations: TranslationEn,
+  },
+  ko: {
+    translations: TranslationKo,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources: resource,
+
+  // 초기 설정 언어
+  lng: "ko",
+  fallbackLng: "ko",
+  debug: true,
+  defaultNS: "translations",
+  ns: "translations",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
