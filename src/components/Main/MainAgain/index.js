0f89c4f312a2570963007dfe947b300830222368
gkseogus <fbzbffldj@gmail.com>
2022-09-07 14:14:57 +0900

    220907_add to MainAgain

--- src/components/Main/MainAgain/index.js
+++ src/components/Main/MainAgain/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import SubtractIcon from "../Img/subtractIcon.svg";
+import Main from "../index";
 
 const Contain = styled.div`
   width: 1194px;
@@ -44,24 +45,79 @@ const SubtractContain = styled.img`
   margin-left: 851px;
 `;
 
+const SelectButton = styled.button`
+  cursor: pointer;
+  width: 212px;
+  height: 65px;
+  border-radius: 100px;
+  border: 0;
+  outline: 0;
+  margin-top: 540px;
+  margin-left: 356px;
+  margin-right: 59px;
+  box-shadow: 3px 5px 10px rgba(32, 33, 36, 0.15);
+  background-color: #cccccc;
+`;
+
+const AnalyzeButton = styled.button`
+  cursor: pointer;
+  width: 212px;
+  height: 65px;
+  border-radius: 100px;
+  border: 0;
+  outline: 0;
+  margin-top: 540px;
+  margin-right: 329px;
+  box-shadow: 3px 5px 10px rgba(32, 33, 36, 0.15);
+  background-color: #1a73e8;
+`;
+
+const ButtonText = styled.p`
+  margin-top: 12px;
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 41px;
+  text-align: center;
+  color: #ffffff;
+`;
+
 const MainAgain = (props) => {
+  const [change, setChange] = useState(false);
+
   const FileDelete = () => {
     props.files.filter((files) => files.key !== props.key);
     console.log("test", props.files);
   };
 
-  console.log("test", props.files);
+  const SelectAgain = useCallback(() => {
+    setChange(!change);
+  }, [change]);
+
   return (
-    <Contain>
-      <FileNameContain>
-        <FileName>{props.files}</FileName>
-        <SubtractContain
-          src={SubtractIcon}
-          alt="subtract button"
-          onClick={FileDelete}
-        />
-      </FileNameContain>
-    </Contain>
+    <div>
+      {change ? (
+        <Main />
+      ) : (
+        <Contain>
+          <FileNameContain>
+            <FileName>{props.files}</FileName>
+            <SubtractContain
+              src={SubtractIcon}
+              alt="subtract button"
+              onClick={FileDelete}
+            />
+          </FileNameContain>
+          <SelectButton>
+            <ButtonText onClick={SelectAgain}>Select Again</ButtonText>
+          </SelectButton>
+          <AnalyzeButton>
+            <ButtonText>Analyze video</ButtonText>
+          </AnalyzeButton>
+        </Contain>
+      )}
+    </div>
   );
 };
 
