e9e8295064a2ff3e5b7c31401be73a2a46db3a37
DogGang <rlaehgusqp@naver.com>
2022-09-09 04:53:25 +0900

    20220909_fix to merge conflict

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import MainNavbar from "./components/MainNavBar";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Main from "./components/Main";
-import Contact from "./components/Contact";
+import AllRountes from "./AllRountes";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <MainNavbar />
-        <Home />
-        <Main />
-        <Contact />
-        <Footer />
-      </BrowserRouter>
+      <AllRountes />
     </div>
   );
 }
