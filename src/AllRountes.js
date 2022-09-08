2b811838e60ecb71b90311d0e21156a42992d445
gkseogus <fbzbffldj@gmail.com>
2022-09-08 15:04:53 +0900

    220908_add to Routes

--- src/AllRountes.js
+++ src/AllRountes.js
@@ -1,14 +1,52 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import HomeNavBar from "./components/HomeNavBar";
+import MainNavBar from "./components/Main/MainNavBar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Main from "./components/Main";
 
 const AllRoutesApp = () => (
   <Router>
     <Routes>
-      {["/Home", "/"].map((path) => {
-        return <Route key={uuidv4()} path={path} element={<div></div>} />;
+      {["/home", "/"].map((path) => {
+        return (
+          <Route
+            key={uuidv4()}
+            path={path}
+            element={
+              <div>
+                <HomeNavBar />
+                <Home />
+                <Footer />
+              </div>
+            }
+          />
+        );
       })}
       ;
+      <Route
+        key={uuidv4()}
+        path={"main"}
+        element={
+          <div>
+            <MainNavBar />
+            <Main />
+            <Footer />
+          </div>
+        }
+      />
+      <Route
+        key={uuidv4()}
+        path={"contact"}
+        element={
+          <div>
+            <MainNavBar />
+            <Footer />
+          </div>
+        }
+      />
     </Routes>
   </Router>
 );
