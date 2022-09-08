4ae9aeb31d7e73d03005f0473e6d4af40da456b2
gkseogus <fbzbffldj@gmail.com>
2022-09-08 17:40:51 +0900

    220908_add to i18next

--- src/components/HomeNavBar/I18n/index.js
+++ src/components/HomeNavBar/I18n/index.js
@@ -1,4 +1,4 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import TranslationEn from "./translatonEn.json";
 import TranslationKo from "./translationKo.json";
@@ -11,20 +11,20 @@ const resource = {
     translations: TranslationKo,
   },
 };
+const I18nextFunc = () => {
+  i18next.use(initReactI18next).init({
+    resources: resource,
 
-i18n.use(initReactI18next).init({
-  resources: resource,
-
-  // 초기 설정 언어
-  lng: "ko",
-  fallbackLng: "ko",
-  debug: true,
-  defaultNS: "translations",
-  ns: "translations",
-  keySeparator: false,
-  interpolation: {
-    escapeValue: false,
-  },
-});
-
-export default i18n;
+    // 초기 설정 언어
+    lng: "ko",
+    fallbackLng: "ko",
+    debug: true,
+    defaultNS: "translations",
+    ns: "translations",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+};
+export default I18nextFunc;
