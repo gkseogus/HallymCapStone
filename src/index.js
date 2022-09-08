10321c10321b0625f754dbc3e48868bab3a37bab
gkseogus <fbzbffldj@gmail.com>
2022-09-08 16:18:03 +0900

    220908_apply to LoadingIndicator

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import LoadingIndicator from "./components/Main/LoadingIndicator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <LoadingIndicator />
   </React.StrictMode>
 );
 
