cf4c6aac170c681ee7dd322fe9ff8b0d6e97eca7
gkseogus <fbzbffldj@gmail.com>
2022-10-05 16:46:15 +0900

    221005_apply to post api

--- Node_HallymCapStone/index.js
+++ Node_HallymCapStone/index.js
@@ -1,41 +1,39 @@
 const cors = require("cors");
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const mysql = require("mysql");
 const PORT = process.env.port || 8000;
-const bodyParser = require("body-parser");
+const ROOT_ID = process.env.root_id;
+const ROOT_PASSWORD = process.env.root_password;
 
 const db = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "gkseogus!",
-  database: "gkseogusblog",
+  user: ROOT_ID,
+  password: ROOT_PASSWORD,
+  database: "downFile",
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: true }));
 
 // api
-app.get("/api/downloadfile", (req, res) => {
-  const sqlQuery = "SELECT *FROM BOARD;";
+app.get("/api/fileList", (req, res) => {
+  const sqlQuery = "SELECT *FROM filetable;";
   db.query(sqlQuery, (err, result) => {
     res.send(result);
   });
 });
 
-// app.post("/api/fileinsert", (req, res) => {
-//   var title = req.body.title;
-//   var register = req.body.register;
-//   var content = req.body.content;
-//   const sqlQuery =
-//     "INSERT INTO BOARD (BOARD_TITLE, REGISTER_ID, BOARD_CONTENT) VALUES (?,?,?);";
-//   db.query(sqlQuery, [title, register, content], (err, result) => {
-//     res.send(result);
-//     console.log(result);
-//     console.log("title:", title, "register:", register, "content:", content);
-//   });
-// });
+app.post("/api/fileinsert", (req, res) => {
+  var file = req.body.file;
+
+  const sqlQuery = "INSERT INTO filetable (DOWNFILE) VALUES (?);";
+  db.query(sqlQuery, [file], (err, result) => {
+    res.send(result);
+    console.log(result);
+    console.log("FILE", file);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
