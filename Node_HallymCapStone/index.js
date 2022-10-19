45f508f914e14d1092d53a22deda68b8751dc6c6
gkseogus <fbzbffldj@gmail.com>
2022-10-19 16:23:29 +0900

    221019_add to mariadb

--- Node_HallymCapStone/index.js
+++ Node_HallymCapStone/index.js
@@ -2,13 +2,13 @@ const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const mysql = require("mysql");
+const mariadb = require("mariadb");
 const PORT = process.env.port || 8000;
 const ROOT_ID = process.env.root_id;
 const ROOT_PASSWORD = process.env.root_password;
 const bodyParser = require("body-parser");
 
-const db = mysql.createPool({
+const db = mariadb.createPool({
   host: "localhost",
   user: ROOT_ID,
   password: ROOT_PASSWORD,
@@ -31,7 +31,7 @@ app.post("/api/insert", (req, res) => {
   console.log(req.body);
   var file = req.body.file;
 
-  const sqlQuery = "INSERT INTO FILETABLE (DOWNFILE) VALUES (?);";
+  const sqlQuery = "INSERT INTO FILETABLE (FILENAME) VALUES (?);";
   db.query(sqlQuery, [file], (err, result) => {
     res.send(result);
   });
