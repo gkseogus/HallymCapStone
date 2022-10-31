033a13f683a0eeae551f4678f128404a16d255a9
gkseogus <fbzbffldj@gmail.com>
2022-10-31 14:50:09 +0900

    221031_add to i18next

--- src/lang/i18n/index.js
+++ src/lang/i18n/index.js
@@ -1,22 +1,31 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import TranslationEn from "./translatonEn.json";
-import TranslationKo from "./translationKo.json";
+import ko from "./ko-KR.json";
+import en from "./en-US.json";
+import cn from "./cn-CH.json";
+import jp from "./jp-JP.json";
 
 const resource = {
-  en: {
-    translations: TranslationEn,
-  },
   ko: {
-    translations: TranslationKo,
+    translations: ko,
+  },
+  en: {
+    translations: en,
+  },
+  ch: {
+    translations: cn,
+  },
+  jp: {
+    translations: jp,
   },
 };
+
 i18n.use(initReactI18next).init({
   resources: resource,
 
   // 초기 설정 언어
-  lng: "ko",
-  fallbackLng: "ko",
+  lng: "en",
+  fallbackLng: "en",
   debug: true,
   defaultNS: "translations",
   ns: "translations",
